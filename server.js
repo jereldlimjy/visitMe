ec7fc2b9128178208714c8192b7c4d7ff7e79b77
haozhi98 <57322708+haozhi98@users.noreply.github.com>
2021-01-09 01:15:37 +0800

    video

--- server.js
+++ server.js
@@ -9,6 +9,44 @@ const User = require('./models/user');
 const axios = require('axios');
 const favicon = require('serve-favicon');
 
+var AccessToken = require('twilio').jwt.AccessToken;
+var VideoGrant = AccessToken.VideoGrant;
+
+// Substitute your Twilio AccountSid and ApiKey details
+var ACCOUNT_SID = 'ACdba73502932bf6e6034a8162bedb56fe';
+var API_KEY_SID = 'SK10a271b81094eec7f06688e422eddcea';
+var API_KEY_SECRET = 'r4dHFbOrXDSsx4bFUzxmsOSPxTBNaUVt';
+
+// Create an Access Token
+var accessToken = new AccessToken(
+  ACCOUNT_SID,
+  API_KEY_SID,
+  API_KEY_SECRET
+);
+
+// Set the Identity of this token
+accessToken.identity = 'hao';
+
+// Grant access to Video
+var grant = new VideoGrant();
+grant.room = 'cool room';
+accessToken.addGrant(grant);
+
+// Serialize the token as a JWT
+var jwt = accessToken.toJwt();
+console.log(jwt);
+
+const { connect } = require('twilio-video');
+
+connect('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2YzYzNhOGJhOWM1ZmZlZWQxYjgwZjkwOGQxODAyNTZlLTE2MTAxMjE1MTIiLCJpc3MiOiJTS2YzYzNhOGJhOWM1ZmZlZWQxYjgwZjkwOGQxODAyNTZlIiwic3ViIjoiQUNkYmE3MzUwMjkzMmJmNmU2MDM0YTgxNjJiZWRiNTZmZSIsImV4cCI6MTYxMDEyNTExMiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoiaGFvIiwidmlkZW8iOnt9fX0.aqV4ky8XRC2TA3ld9l_J1CvaoTXtP3kkiFWwPOoHRo0', { name:'my-new-room' }).then(room => {
+  console.log(`Successfully joined a Room: ${room}`);
+  room.on('participantConnected', participant => {
+    console.log(`A remote Participant connected: ${participant}`);
+  });
+}, error => {
+  console.error(`Unable to connect to Room: ${error.message}`);
+});
+
 mongoose.connect('mongodb://localhost:27017/visitme', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
