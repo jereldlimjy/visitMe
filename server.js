81e01bcf4357c6a9abf79396faeffbcbfded3021
Jereld Lim <jereldlimjy@hotmail.com>
2021-01-08 12:43:05 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+  res.send('this is the landing page');
+})
+
+app.get('/home', (req, res) => {
+  res.render('index');
+})
+
+app.listen(3000, () => {
+  console.log("SERVER IS UP!");
+})
